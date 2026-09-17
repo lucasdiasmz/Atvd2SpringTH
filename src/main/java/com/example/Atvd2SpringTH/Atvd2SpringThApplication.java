e3adc1bb64a92ae041706c1d46aa7b8caf860ed0
Lucas Dias <lucasdiasmelo4@gmail.com>
2026-09-17 16:50:01 -0300

    Packages

---
+++ src/main/java/com/example/Atvd2SpringTH/Atvd2SpringThApplication.java
@@ -0,0 +1,13 @@
+package com.example.Atvd2SpringTH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atvd2SpringThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atvd2SpringThApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Atvd2SpringTH/Atvd2SpringThApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Atvd2SpringTH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atvd2SpringThApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
